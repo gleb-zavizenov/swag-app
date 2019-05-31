acbe30a5ec2a7572a706f22112d86cde7dc5faae
Gleb Zavizenov <gleb.zavizenov@gmail.com>
2019-05-30 20:06:54 -0400

    Added custom collectionView cells to the main.storyboard

---
+++ coder-swag/coder-swag/View/ProductViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductViewCell.swift
+//  coder-swag
+//
+//  Created by Gleb Zavizenov on 2019-05-30.
+//  Copyright © 2019 Gleb Zavizenov. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ProductImage: UIImageView!
+    @IBOutlet weak var ProductTitle: UILabel!
+    @IBOutlet weak var ProductPrice: UILabel!
+    
+}
